7a177f14bfe8a9ceadf6c7bb175645961460bc3f
William Bergen <wbergen@lynk.world>
2023-04-16 15:21:57 -0400

    Added command-line argments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::path::PathBuf;
+use clap::{Parser,Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    /// Input image
+    input_image: PathBuf,
+    /// Output image
+    output_image: PathBuf,
+    /// Number of threads
+    #[arg(short = 'j', long = "num-threads", value_name = "NUM_THREADS", default_value_t = 1)]
+    num_threads: u8,
+    #[command[subcommand]]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adjust the exposure of the image
+    Exposure { 
+        /// Exposure adjustment amount
+        amount: f64 
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
